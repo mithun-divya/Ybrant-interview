55b7927e70d142f55c11947580406fa7d07433e9
divya-vegesna <125994104+divya-vegesna@users.noreply.github.com>
2023-07-28 11:08:00 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'NodeJSApplication-Sample',
 	    'sonar.projectKey':'NodeJsApplication',
-	    'sonar.login': 'squ_eda6844c1b2cd3a68a529098c81bc58ab08863ae',
+	    'sonar.login': 'squ_f313915510467516add95f5f6ea6948147998589',
             'sonar.projectVersion':'1.0',
 	          'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
